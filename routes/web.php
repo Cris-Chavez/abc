e4869d137a9ea191688e58a4c8de15550c4f7d45
Cristian Chavez <crist.reu1990@gmail.com>
2021-07-11 17:59:47 -0600

    Add group middleware Auth y cambio de ruta / de welcome a auth.login

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,24 @@ use App\Http\Controllers\ContainerController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('user', UserController::class );
-Route::resource('provider', ProviderController::class );
-Route::resource('destination', DestinationController::class);
-Route::get('product/catalogue', [App\Http\Controllers\ProductController::class, 'catalogue'])->name('product.catalogue');
-Route::get('product/search', [App\Http\Controllers\ProductController::class, 'search'])->name('product.search');
-Route::resource('product', ProductController::class);
-Route::get('price/get', [PriceController::class, 'getPrices'])->name('price.getPrices');
-Route::resource('price', PriceController::class);
-Route::get('container/getProvider', [App\Http\Controllers\ContainerController::class, 'getProvider'])->name('container.getProviders');
-Route::resource('container', ContainerController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('user', UserController::class );
+    Route::resource('provider', ProviderController::class );
+    Route::resource('destination', DestinationController::class);
+    Route::get('product/catalogue', [App\Http\Controllers\ProductController::class, 'catalogue'])->name('product.catalogue');
+    Route::get('product/search', [App\Http\Controllers\ProductController::class, 'search'])->name('product.search');
+    Route::resource('product', ProductController::class);
+    Route::get('price/get', [PriceController::class, 'getPrices'])->name('price.getPrices');
+    Route::resource('price', PriceController::class);
+    Route::get('container/getProvider', [App\Http\Controllers\ContainerController::class, 'getProvider'])->name('container.getProviders');
+    Route::resource('container', ContainerController::class);
+    
+});
+
+
